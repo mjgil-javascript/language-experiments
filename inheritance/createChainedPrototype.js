496e9a7cc869f0addca3cd8f8e19837132b83986
Malcom Gilbert <malcomgilbert@gmail.com>
2018-01-06 19:55:19 -0500

    finish experiments

--- inheritance/createChainedPrototype.js
+++
@@ -1,5 +0,0 @@
-export const makeChainedPrototype = (assignTo, assignFrom) => {
-  const chainedPrototype = Object.assign(Object.create(assignTo.prototype), assignFrom.prototype)
-  const factory = (...args) => { return Object.assign(Object.create(chainedPrototype), assignTo(...args)) }
-  return factory
-}
--- inheritance/createClass.js
+++ inheritance/createClass.js
@@ -1,6 +1,39 @@
-export default function createClass(ctor, superClass) {
+function createClass(ctor, superClass) {
   if (superClass) {
     ctor.prototype = Object.create(superClass.prototype);
   }
   ctor.prototype.constructor = ctor;
 }
+
+function MyObject() {}
+MyObject.prototype.one = function() {return 'myObject'}
+MyObject.prototype.two = function() {return 'myObject-two'}
+
+createClass(MyArray, MyObject);
+function MyArray() {this.test = 1}
+MyArray.test = function() {return 'three'};
+MyArray.prototype.one = function() {return 'hi'};
+
+var arr = new MyArray()
+console.log(arr)
+console.log(arr.test)
+console.log(arr.one())
+console.log(arr.two())
+
+
+// to get rid of the "new" requirement, use instanceof
+createClass(MyArray2, MyObject);
+function MyArray2() {
+  if (!(this instanceof MyArray2)) {
+    return new MyArray2();
+  }
+  this.test = 1
+}
+MyArray2.test = function() {return 'three'};
+MyArray2.prototype.one = function() {return 'hi'};
+
+var arr2 = MyArray2()
+console.log(arr2)
+console.log(arr2.test)
+console.log(arr2.one())
+console.log(arr2.two())
